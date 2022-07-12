8dfe2cf85b213dfa149c194d9dfcb6614784d8fd
polukar <polukarov.qweewq@gmail.com>
2022-07-12 22:50:24 +0300

    update

--- src/js/project/video.js
+++ src/js/project/video.js
@@ -1,6 +1,7 @@
 
 const reportSlider = document.querySelector('.reports-slider ');
 const reportSlide = document.querySelectorAll('.report-slide');
+
 const fadeOut = (el, timeout) => {
   el.style.opacity = 1;
   el.style.transition = `opacity ${timeout}ms`;
@@ -21,72 +22,70 @@ const fadeIn = (el, timeout, display) => {
 };
 
 if (reportSlide) {
-  window.onload = function () {
-    let stopVideos = (ind) => {
-      reportSlide.forEach((elem, index) => {
-        let buttonPlay = elem.querySelector('.report__play');
-        let video = elem.querySelector('video');
-        let trigger = elem.querySelector('.report-slide__trigger')
-        video.pause();
-        video.load();
-        video.removeAttribute('controls');
-        trigger.classList.remove('--fade');
-        if (ind != index) {
-          fadeIn(buttonPlay, 300)
-        }
-        elem.classList.remove('--is-active');
-      })
-    };
+  let stopVideos = (ind) => {
+    reportSlide.forEach((elem, index) => {
+      let buttonPlay = elem.querySelector('.report__play');
+      let video = elem.querySelector('video');
+      let trigger = elem.querySelector('.report-slide__trigger')
+      video.pause();
+      video.load();
+      video.removeAttribute('controls');
+      trigger.classList.remove('--fade');
+      if (ind != index) {
+        fadeIn(buttonPlay, 300)
+      }
+      elem.classList.remove('--is-active');
+    })
+  };
 
-    window.addEventListener('click', function (e) {
-      let target = e.target || e.srcElement || e.currentTarget
-      if (!reportSlider.contains(target)) {
-        stopVideos(100);
+  window.addEventListener('click', function (e) {
+    let target = e.target || e.srcElement || e.currentTarget
+    if (!reportSlider.contains(target)) {
+      stopVideos(100);
+    }
+  });
+
+  reportSlide.forEach((elem, index) => {
+    let buttonPlay = elem.querySelector('.report__play');
+    let video = elem.querySelector('.report__video video');
+    let trigger = elem.querySelector('.report-slide__trigger');
+    let close = elem.querySelector('.slider__close ');
+
+
+    elem.addEventListener('click', () => {
+      if (!elem.classList.contains('--is-active')) {
+        stopVideos(index);
+        trigger.classList.add('--fade')
+        fadeOut(buttonPlay, 300);
+        video.setAttribute('controls', true);
+        video.muted = false;
+        elem.classList.add('--is-active');
+        setTimeout(() => {
+          video.currentTime = 0;
+          video.play();
+        }, 200)
       }
     });
 
-    reportSlide.forEach((elem, index) => {
-      let buttonPlay = elem.querySelector('.report__play');
-      let video = elem.querySelector('.report__video video');
-      let trigger = elem.querySelector('.report-slide__trigger');
-      let close = elem.querySelector('.slider__close ');
+    close.addEventListener('click', () => {
+      video.load();
+      setTimeout(() => {
+        stopVideos(100);
+      }, 100)
+    })
 
-
-      elem.addEventListener('click', () => {
-        if (!elem.classList.contains('--is-active')) {
-          stopVideos(index);
-          trigger.classList.add('--fade')
-          fadeOut(buttonPlay, 300);
-          video.setAttribute('controls', true);
-          video.muted = false;
-          elem.classList.add('--is-active');
-          setTimeout(() => {
-            video.currentTime = 0;
-            video.play();
-          }, 200)
-        }
+    if (!elem.classList.contains('--is-active')) {
+      trigger.addEventListener('mouseover', () => {
+        video.muted = true;
+        video.play();
       });
 
-      close.addEventListener('click', () => {
+      trigger.addEventListener('mouseout', () => {
         video.load();
-        setTimeout(() => {
-          stopVideos(100);
-        }, 100)
-      })
-
-      if (!elem.classList.contains('--is-active')) {
-        trigger.addEventListener('mouseover', () => {
-          video.muted = true;
-          video.play();
-        });
-
-        trigger.addEventListener('mouseout', () => {
-          video.load();
-        });
-      }
+      });
+    }
 
 
-    })
-  }
+  })
 }
 
